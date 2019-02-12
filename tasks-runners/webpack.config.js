18e374ba0570f9d932171bf60f0765c9be6578ae
Ruslan Kutynko <kutynko@hotmail.com>
2019-02-12 17:22:32 +0300

    more examples

---
+++ tasks-runners/webpack.config.js
@@ -0,0 +1,19 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  // Add the JSHint loader
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Run the loader on all .js files
+        exclude: /node_modules/, // ignore all files in the node_modules folder
+        use: "jshint-loader"
+      }
+    ]
+  }
+};
